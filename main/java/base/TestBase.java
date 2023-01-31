df006a958ff4afaa88f3a1254d27229740418fdb
neha.sharma <neha.sharma@enercross.com>
2023-01-31 02:00:15 -0600

    second commit

---
+++ main/java/base/TestBase.java
@@ -0,0 +1,60 @@
+package base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+    public static Properties prop;
+    protected static WebDriver driver;
+
+    public TestBase()
+    {
+        try{
+           String path= System.getProperty("user.dir")+ File.separator+"src"+File.separator+"main"+File.separator+"java"+File.separator+"config"+File.separator+"Config.properties";
+            FileInputStream fis=new FileInputStream(path);
+             prop=new Properties();
+             prop.load(fis);
+            }catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void intialization()
+    {
+        String browsername=prop.getProperty("browser");
+        String Url=prop.getProperty("url") ;
+        if(browsername.equalsIgnoreCase("chrome"))
+        {
+            WebDriverManager.chromedriver().setup();
+            driver=new ChromeDriver();
+        }else if(browsername.equalsIgnoreCase("firefox"))
+        {
+            WebDriverManager.firefoxdriver().setup();
+            driver=new FirefoxDriver();
+        }else {
+            System.out.println("Please use chrome or firefox browser");
+        }
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+
+
+    }
+
+
+
+}
